8dfc2e1333a8b57f9773842afb0665a44b87e681
Driky <chatelain.cedric.pro@gmail.com>
2020-02-19 23:04:08 -0500

    Everythiong works only if we modify customize-cra and remove localIdentName

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { Button } from "antd";
+import styles from "./App.module.less";
 
 const App = () => {
   return (
-    <main
-      style={{
-        height: "500px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
+    <main className={styles.app}>
       <Button type="primary">Hello, Ant Design!</Button>
       <a href="foo.bar">I'm a link. Click me, please!</a>
     </main>
