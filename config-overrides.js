181391b4cad6107b53ae12f828654109542fb2ac
Driky <chatelain.cedric.pro@gmail.com>
2020-02-20 19:19:57 -0500

    Added basic menu with naviguation. And some linting.

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,21 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
   }),
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
-      "@primary-color": "#f48549", // customize as needed
-      "@link-color": "#e6a07c", // customize as needed
-      "@font-size-base": "18px" // customize as needed
-    }
+      '@primary-color': '#f48549', // customize as needed
+      '@link-color': '#e6a07c', // customize as needed
+      '@font-size-base': '18px', // customize as needed
+    },
   })
 );
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { FunctionComponent, useState } from 'react';
+import { Button, Menu, Icon } from 'antd';
 import styles from './App.module.less';
 
-const App = () => {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+const App: FunctionComponent<{ initialKey?: string }> = ({ initialKey = 'home' }): JSX.Element => {
+  const [currentKey, setCurrentKey] = useState(initialKey);
+
+  const handleClick = (e: any) => {
+    console.log('click ', e);
+    setCurrentKey(e.key);
+  };
+
+  return (
+    <Router>
+      <div>
+        <Menu onClick={handleClick} selectedKeys={[currentKey]} mode="horizontal">
+          <Menu.Item key="home">
+            <a href="./home" target="_blank" rel="noopener noreferrer">
+              <Icon type="home" />
+              Home
+            </a>
+          </Menu.Item>
+          <Menu.Item key="about">
+            <a href="./about" target="_blank" rel="noopener noreferrer">
+              <Icon type="question" />
+              About
+            </a>
+          </Menu.Item>
+        </Menu>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+function Home(): JSX.Element {
   return (
     <main className={styles.app}>
       <Button type="primary">Hello, Ant Design!</Button>
-      <a href="foo.bar">I'm a link. Click me, please!</a>
+      <a href="About">I'm a link. Click me, please!</a>
     </main>
   );
-};
+}
+
+function About(): JSX.Element {
+  return <h2>About</h2>;
+}
 
 export default App;
